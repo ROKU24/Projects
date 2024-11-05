66dd57ed0ad9116e08364cfb0e84b24086e5083f
ROKU24 <96464268+ROKU24@users.noreply.github.com>
2024-11-05 10:26:53 +0000

    Simple add friend button

---
+++ FriendRequestButton/script.js
@@ -0,0 +1,23 @@
+var iStatus=document.querySelector('h5')
+var btn= document.querySelector('#add')
+var check=0
+
+btn.addEventListener('click',function () {
+    if (check==0) {
+        iStatus.innerHTML='Friends'
+        iStatus.style.color='green'
+        btn.innerHTML='Remove Friend'
+        btn.style.backgroundColor='gray' 
+        check=1   
+        
+    }
+    else{
+        iStatus.innerHTML='Stranger'
+        iStatus.style.color='red'
+        btn.innerHTML='Add Friend' 
+        btn.style.backgroundColor='cadetblue'
+        check=0
+    }
+
+})
+
